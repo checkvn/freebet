be185e2d879912aac47b92dd9d3910d499bcf1ac
checkvn <lap.fptaptech@gmail.com>
2016-04-27 11:48:45 +0700

    Commit test.java file

---
+++ freebet/src/freebet/test.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freebet;
+
+/**
+ *
+ * @author Lapcute
+ */
+public class test {
+    
+    private String username;
+
+    /**
+     * Get the value of username
+     *
+     * @return the value of username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @param username new value of username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
